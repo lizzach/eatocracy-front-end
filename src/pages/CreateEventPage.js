9cb38653b5b126aeb6fbf9e14ee5c4c1d4ae0db3
Elizabeth Zachary <elizabethzachary96@gmail.com>
2023-08-02 17:02:05 -0700

    react router implemented on react-router-test branch

---
+++ src/pages/CreateEventPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateEventPage = () => {
+    return(
+        <div>
+            <h1>This is the CreateEventPage</h1>
+        </div>
+    )
+};
+
+export default CreateEventPage;
